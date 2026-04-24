421d74f4b45c2c852012ef0364e14639dd199e4b
oXbLiXxX <97739416+oxblixxx@users.noreply.github.com>
2026-04-24 23:03:23 +0100

    Remove password from Redis connection
    Removed password parameter from Redis connection.

--- worker/worker.py
+++ worker/worker.py
@@ -19,7 +19,6 @@ signal.signal(signal.SIGINT, shutdown)
 r = redis.Redis(
     host=os.getenv("REDIS_HOST", "localhost"),
     port=int(os.getenv("REDIS_PORT", 6379)),
-    password=os.getenv("REDIS_PASSWORD"),
     decode_responses=True
 )
 
